8735dd27323e58899c7205165e5bdda857406ba4
Sourish Sarmah <sourishsarmah@gmail.com>
2019-05-16 02:02:34 +0530

    Added function for replacing invalide and duplicate values in dataset

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,12 @@
+import numpy as np
 
-def replace_yn(df):
-    df = df.replace(to_replace='Yes', value=1)
-    df = df.replace(to_replace='No', value=0)
+def replace(df):
+    # Replace Select with NaN
+    df = df.replace(to_replace='Select', value=np.nan)
+
+    # Merge Similar Labels
+    df['Lead Source'] = df['Lead Source'].replace('google', 'Google')
+    df['Lead Source'] = df['Lead Source'].replace(['blog', 'WeLearn', 'welearnblog_Home'], 'Blog')
+    df['Country'] = df['Country'].replace(['unknown'], np.nan)
+
     return df
